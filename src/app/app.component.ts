5da3e0120073e95e2fdcdeb631309ae296cb493f
Mateo Ber <mateobergoglio6@gmail.com>
2021-06-14 12:41:11 -0300

    control dados

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice4.png';
+  numero1: number;
+  numero2: number;
+
+  constructor(){
+    this.numero1 = 1;
+    this.numero2 = 2;
+  }
+
+  tirarDados(): void {
+    this.numero1 = Math.round( Math.random() * 5) + 1;
+    this.numero2 = Math.round( Math.random() * 5) + 1;
+    this.dadoIzquierda = '../assets/img/dice' + this.numero1 + '.png  ';
+    this.dadoDerecha = '../assets/img/dice' + this.numero2 + '.png';
+  }
+}
